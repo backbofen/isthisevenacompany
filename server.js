7a9974b9110a139a21569338415da0fbc724bfa3
backbofen <michasbuisness@gmail.com>
2020-08-22 13:44:58 +0200

    first rel

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-
+var path = require('path');
 // set port
 var port = process.env.PORT || 8080
 
-app.use(express.static('public'));
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/test.html"));
+});
+
+app.listen(port, function() {
+    console.log("app running on port " + port);
+}) 
