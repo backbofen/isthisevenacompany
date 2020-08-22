571d31ee3d34c2849dc2b1aaaf23b8dae4a6167f
backbofen <michasbuisness@gmail.com>
2020-08-22 22:55:47 +0200

    hell

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const router = express.Router();
+app.use(express.static(__dirname + '/public'));
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname + 'public/index.html'));
-  //__dirname : It will resolve to your project folder.
+app.get('/', function(req, res){
+    res.render('index.html');
 });
-
-router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname + 'public/about.html'));
-});
-
-router.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname + 'public/sitemap.html'));
-});
-
-//add the router
-app.use('/', router);
+//add the router 
 app.listen(process.env.port || 8080);
 
 console.log('Running at Port 8080');
