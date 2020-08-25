d0c7abc862cba0b1f59eaa0c6b8ed51715d16ba0
backbofen <michasbuisness@gmail.com>
2020-08-26 00:18:54 +0200

    h

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/public'));
 
+app.set('view engine', 'html');
+
+app.engine('html', require('ejs').renderFile);
+
 app.get('/', function(req, res){
     res.render('index.html');
 });
 
+app.get('/collections', function(req, res){
+    res.render('collections.html');
+});
 
-//add the router 
 app.listen(process.env.PORT || 8080);
 
 console.log('Running at Port 8080');
