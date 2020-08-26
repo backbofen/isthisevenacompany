ee1fa94d4771676e0b90f5abc3e4b5ed196c5d9f
backbofen <michasbuisness@gmail.com>
2020-08-26 10:44:52 +0200

    g

--- public/js/script.js
+++ public/js/script.js
@@ -5,11 +5,14 @@ const navSlide = () => {
 
   burger.addEventListener("click", () =>{
     nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index)=>{
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+      }
   });
-
-  navLinks.forEach((link, index)=>{
-    
-
   });
 
 }
