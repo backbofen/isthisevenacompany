88826f27528d9836bf7c6acc0329f6c61f2fb6f2
backbofen <michasbuisness@gmail.com>
2020-08-26 02:08:02 +0200

    gg

--- server.js
+++ server.js
@@ -14,13 +14,16 @@ app.get('/collections', function(req, res){
     res.render('collections.html');
 });
 
-app.get('/shop', function(req, res){
-    res.render('shop.html')
+app.get('/store', function(req, res){
+    res.render('store.html')
 });
 
 app.get('/about', function(req, res){
     res.render('about.html')
 });
+app.get('/previews', function(req, res){
+    res.render('previews.html')
+})
 
 app.listen(process.env.PORT || 8080);
 
