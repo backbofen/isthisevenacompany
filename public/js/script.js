0ae8d6c0e6245637e41c22e5ed207e2add54177a
backbofen <michasbuisness@gmail.com>
2021-09-28 15:44:36 +0200

    hihih

--- public/js/script.js
+++ public/js/script.js
@@ -2,21 +2,34 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
+  
+ // const nav = document.getElementsByTagName('ul')[0];
 //toggle nav
   burger.addEventListener("click", () =>{
     nav.classList.toggle("nav-active");
-//animate links  
+
+//kp 
+  //  nav.classList.add('transition');
+
+//animate links
     navLinks.forEach((link, index)=>{
       if (link.style.animation) {
         link.style.animation = "";
       } else {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
       }
-  });
+      });
   //burger animation
   burger.classList.toggle("toggle");
   });
 
+ // window.addEventListener('resize',
+   // () => {
+     // if (window.innerWidth > 1035) {
+       // nav.classList.remove('transition');
+     // }},
+   // false
+ // );
 }
 
 navSlide();
