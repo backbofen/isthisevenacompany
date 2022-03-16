d39ca76a49626d32e46e8542aab36a6a0c6fd2b6
backbofen <michasbuisness@gmail.com>
2022-03-16 12:36:07 +0100

    test

--- server.js
+++ server.js
@@ -10,40 +10,8 @@ app.get('/', function(req, res){
     res.render('index.html');
 });
 
-app.get('/term', function(req, res){
-    res.render('term.html');
-});
-
-app.get('/tsql', function(req, res){
-    res.render('tsql.html')
-});
-
-app.get('/ttransformierung', function(req, res){
-    res.render('ttransformierung.html')
-});
-
-app.get('/tsmodell', function(req, res){
-    res.render('tsmodell.html')
-});
-
 app.get('/earne', function(req, res){
     res.render('earne.html')
-});
-
-app.get('/emicha', function(req, res){
-    res.render('emicha.html')
-});
-
-app.get('/teklausuren', function(req, res){
-    res.render('teklausuren.html')
-});
-
-app.get('/teleks', function(req, res){
-    res.render('teleks.html')
-});
-
-app.get('/umontag', function(req, res){
-    res.render('umontag.html')
 });
 
 app.listen(process.env.PORT || 8080);
