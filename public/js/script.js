1a6358484b3971e303d03ba6099cfa071cf182d7
backbofen <michasbuisness@gmail.com>
2022-03-22 11:55:36 +0100

    test

--- public/js/script.js
+++ public/js/script.js
@@ -1,48 +1,16 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-  
- // const nav = document.getElementsByTagName('ul')[0];
-//toggle nav
-  burger.addEventListener("click", () =>{
-    nav.classList.toggle("nav-active");
+var text = document.getElementById('text');
+var newDom = '';
+var animationDelay = 6;
 
-//kp 
-  //  nav.classList.add('transition');
-
-//animate links
-    navLinks.forEach((link, index)=>{
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
-      }
-      });
-  //burger animation
-  burger.classList.toggle("toggle");
-  });
-
- // window.addEventListener('resize',
-   // () => {
-     // if (window.innerWidth > 1035) {
-       // nav.classList.remove('transition');
-     // }},
-   // false
- // );
+for(let i = 0; i < text.innerText.length; i++)
+{
+    newDom += '<span class="char">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i])+ '</span>';
 }
 
-navSlide();
+text.innerHTML = newDom;
+var length = text.children.length;
 
-
-//function tmouseOver() {
-  //document.getElementById("testsdiv").style.visibility = "hidden";
-  //document.getElementById("erstellerdiv").style.visibility = "hidden";
-  //document.getElementById("unterrichtszeitendiv").style.visibility = "hidden";
-//}
-
-//function tmouseOut() {
-  //document.getElementById("testsdiv").style.opacity = 1;
-  //document.getElementById("erstellerdiv").style.opacity = 1;
-  //document.getElementById("unterrichtszeitendiv").style.opacity = 1;
-//}
+for(let i = 0; i < length; i++)
+{
+    text.children[i].style['animation-delay'] = animationDelay * i + 'ms';
+}
